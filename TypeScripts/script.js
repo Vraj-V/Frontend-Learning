ee19dd133f95ea399278f956a2c1ace89b5849a8
Vraj-V <vrajv2108@gmail.com>
2026-02-20 21:50:09 +0530

    Frontend

---
+++ TypeScripts/script.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello, TypeScript!");
+var num = 42;
+console.log("The number is: " + num);
+//# sourceMappingURL=script.js.map
---
+++ TypeScripts/script.ts
@@ -0,0 +1,4 @@
+console.log("Hello, TypeScript!");
+var num : number = 42;
+
+console.log("The number is: " + num);
