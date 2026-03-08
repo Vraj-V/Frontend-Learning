ca79869f061774939cfa49a35308973fc2f1fd2a
Vraj-V <vrajv2108@gmail.com>
2026-03-08 23:58:46 +0530

    Quiz

--- Quiz_App/quiz/src/components/Quiz/Quiz.jsx
+++ Quiz_App/quiz/src/components/Quiz/Quiz.jsx
@@ -1,20 +1,47 @@
 import React,{useState} from "react";
 import './Quiz.css'
+import {data} from '../../assets/data';
+
 const Quiz = ()=>{
+    const [index,setIndex] = useState(0);
+    const [question,setQuestion] = useState(data[index]);
+
+
+    const checkAns =(e,ans) =>{
+        if(question.ans === ans){
+            e.target.classList.add("correct");
+            setAnwer(ans);
+        }else{
+            e.target.classList.add("wrong");
+           
+        }
+    }
+
+    const pagination =()=>{
     
-    
+            setIndex(index+1);
+            setQuestion(data[index+1]);
+            let li = document.querySelectorAll("li");
+            li.forEach((li)=>{
+                li.classList.remove("correct");
+                li.classList.remove("wrong");
+            })
+    }
     return(
         <>
             <div className="container">
                 <h1>Quiz App</h1>
             <hr />
-            <h2>What is sunday special?</h2>
+            <h2>{index+1} . {question.question} </h2>
             <ul>
-                <li>Food</li>
-                <li>game</li>
-                <li>Party</li>
+                <li onClick={(e)=>{ checkAns(e,1)}}>{question.option1}</li>
+                <li onClick={(e)=>{ checkAns(e,2)}}>{question.option2}</li>
+                <li onClick={(e)=>{ checkAns(e,3)}}>{question.option3}</li>
+                <li onClick={(e)=>{ checkAns(e,4)}}>{question.option4}</li>
             </ul>
-            <button>Next</button>
+            <button onClick={()=>{
+                pagination()
+            }}>Next</button>
             <div className="index"> 1 of 5 Questions</div>
             </div>
         </>
